ad738c8903a6ef37b081dec677f408b4ae18d64e
JasonAlpha-MODDER <64051651+JasonAlpha-MODDER@users.noreply.github.com>
2020-11-08 19:17:42 +0800

    Create ClientProxy.java

---
+++ src/mian/java/com/github/alphacode/alphacore/client/ClientProxy.java
@@ -0,0 +1,38 @@
+package com.github.alphacode.alphacore.client;
+import com.github.alphacode.alphacore.common.CommonProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+import com.github.alphacode.alphacore.common.CommonProxy;
+
+
+
+public class ClientProxy extends CommonProxy
+{
+    @Override
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        super.preInit(event);
+    }
+
+    @Override
+    public void init(FMLInitializationEvent event)
+    {
+        super.init(event);
+    }
+
+    @Override
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        super.postInit(event);
+    }
+
+
+
+
+
+
+
+}
+
