08f77a53ae8d085f31e91f816944dd0e89d1a870
JasonAlpha-MODDER <64051651+JasonAlpha-MODDER@users.noreply.github.com>
2020-11-08 19:07:50 +0800

    Create AlphaCore.java

---
+++ src/mian/java/com/github/jasonalpha/alphacore/AlphaCore.java
@@ -0,0 +1,40 @@
+package com.github.jasonalpha.alphacore;
+
+import com.github.jasonalpha.alphacore.common.CommonProxy;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+@Mod(modid = AlphaCore.MODID, name = AlphaCore.NAME, version = AlphaCore.VERSION)
+public class AlphaCore
+
+{
+    public static final String MODID = "alphacore";
+    public static final String NAME = "AlphaCore";
+    public static final String VERSION = "0.0.1";
+
+    private static Logger logger;
+
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        proxy.preInit(event);
+
+    }
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        proxy.init(event);
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        proxy.postInit(event);
+    }
+    @SidedProxy(clientSide = "com.github.jasonalpha.alphacore.client.ClientProxy",
+            serverSide = "com.github.jasonalpha.alphacore.common.CommonProxy")
+    public static CommonProxy proxy;}
