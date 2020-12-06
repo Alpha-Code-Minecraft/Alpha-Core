ee86c25914c5795909eadac9623d2cafcc026032
Jason-Alpha <64051651+JasonAlpha-MODDER@users.noreply.github.com>
2020-12-06 15:59:03 +0800

    Update and rename MainMethods to MainMethods.java

---
+++ src/mian/java/com/github/alphacode/alphacore/api/methods/MainMethods.java
@@ -0,0 +1,23 @@
+package com.github.alphacode.alphacore.api.methods;
+
+
+/**
+ * @author AlphaCore:JasonAlpha
+ *
+ */
+
+public class MainMethods {
+  
+  public void ModWarnList(int ExampleWarn) {
+       //此方法可用于显示模组冲突信息
+        int x = ExampleWarn;
+
+        System.out.println(" 可能与此模组冲突的模组：" + ExampleWarn);
+    }
+  
+  
+      
+
+
+      }
+
