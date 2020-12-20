76b2240694af5b867e739a960b4404ed59be4fb5
Jason-Alpha <64051651+JasonAlpha-MODDER@users.noreply.github.com>
2020-12-20 20:16:40 +0800

    Update _calculator_.py

--- src/mian/java/com/github/alphacode/alphacore/libs/pylibs/calculation/_calculator_.py
+++ src/mian/java/com/github/alphacode/alphacore/libs/pylibs/calculation/_calculator_.py
@@ -1,7 +1,32 @@
+def input():
+    pass
+def print(param):
+    pass
+def x(args):
+    pass
+def y(args):
+    pass
+
 def add():
-    x = input()
-    y = input()
+    input()
+    input()
     print(x + y)
-
-
 add()
+
+def reduce():
+    input()
+    input()
+    print(x - y)
+reduce()
+
+def multiply():
+    input()
+    input()
+    print(x * y)
+multiply()
+
+def subtract():
+    input()
+    input()
+    print(x / y)
+subtract()
